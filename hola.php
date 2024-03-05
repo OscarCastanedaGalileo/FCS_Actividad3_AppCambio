5d03cbf761f93e78431386af643b96589c0ba795
Oscar Castaneda <07144106@galileo.edu>
2024-03-05 01:24:33 +0000

    Second commit

--- hola.php
+++ hola.php
@@ -2,4 +2,4 @@
     echo "<h1>Hola mundo, soy Oscar André Castañeda García</h1>";
 ?>
 
-<img src="Oscar1.jpg" alt="Oscar programando en una montaña" width="500" height="600"> 
+<img src="Oscar1.jpg" alt="Oscar programando en una montaña" width="1000" height="1200"> 
